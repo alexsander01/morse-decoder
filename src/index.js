83d17baaf8f715d57559c698f534e5de7ae0c5af
Alexsander Brantsevich <lexsander.red.34@gmail.com>
2020-03-23 23:26:03 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -39,6 +39,27 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const array = expr.match(/.{1,10}/g);
+    
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] == '**********') {
+            array[i] = '++';
+        } else {
+            array[i] = parseInt(array[i]).toString();
+        }
+
+        array[i] = array[i].match(/.{1,2}/g);
+        for(let j = 0; j < array[i].length; j++) {
+            switch(array[i][j]) {
+                case '10': array[i][j] = '.'; break;
+                case '11': array[i][j] = '-'; break;
+                case '++': array[i][j] = ' '; break;
+            }
+        }
+        if(array[i].join('') != ' ') array[i] = MORSE_TABLE[array[i].join('')];
+    }
+    
+    return array.join('');
 }
 
 module.exports = {
